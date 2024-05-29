74337d46affea6aecedc33197afe5caad8c7a68e
apalmer515 <apalmer@515tech.com>
2024-05-29 01:17:22 -0500

    add email service

--- resources/user.py
+++ resources/user.py
@@ -26,7 +26,7 @@ def send_simple_message(to, subject, body):
     return requests.post(
 		f"https://api.mailgun.net/v3/{domain}/messages",
 		auth=("api", os.getenv("MAILGUN_API_KEY")),
-		data={"from": "ap <mailgun@{domain}>",
+		data={"from": f"ap <mailgun@{domain}>",
 			"to": [to],
 			"subject": subject,
 			"text": body})
